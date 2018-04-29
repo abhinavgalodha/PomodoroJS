1f24af074dba045f1b46781fd1b32a0ada82288f
Galodha <v744728@global.ual.com>
2018-04-29 15:55:56 -0500

    Updated new JS

---
+++ main.1.js
@@ -0,0 +1,78 @@
+(function (window) {
+
+    var minutesDefault = 24;
+    var secondsDefault = 59;
+
+    var currentMinutes;
+    var currentSeconds;
+    var intervalCallback;
+
+    var btnStart;
+    var btnStop;
+    var btnReset;
+
+    setDefaultValues();
+
+    function setDefaultValues() {
+        currentMinutes = minutesDefault;
+        currentSeconds = secondsDefault;
+        updateUI();
+    }
+
+    function loadDefaultValues(){
+        
+    }
+
+
+    function updateTime() {
+        currentSeconds = currentSeconds - 1;
+        if (currentSeconds === 0) {
+            currentSeconds = secondsDefault;
+            currentMinutes = currentMinutes - 1;
+            if (currentMinutes === 0) {
+                currentMinutes = minutesDefault;
+            }
+        }
+        updateUI();
+    }
+
+    window.start = function() {
+        disableStartBtn(true);
+        disableStopBtn(false);
+        disableResetBtn(false);
+        intervalCallback = setInterval(updateTime, 1000);
+    }
+
+    window.stop = function() {
+        disableStartBtn(false);
+        disableStopBtn(true);
+        disableResetBtn(false);
+        clearInterval(intervalCallback);
+    }
+
+    window.reset = function() {
+        setDefaultValues();
+        disableStartBtn(false);
+        disableStopBtn(false);
+        disableResetBtn(true);
+        clearInterval(intervalCallback);
+    }
+
+    function updateUI() {
+        document.querySelector("#spanMinutes").textContent = currentMinutes;
+        document.querySelector("#spanSeconds").textContent = currentSeconds;
+    }
+
+    function disableStartBtn(disabled){
+        document.querySelector("#btnStart").disabled = disabled;
+    }
+
+    function disableStopBtn(disabled){
+        document.querySelector("#btnStop").disabled =disabled;
+    }
+
+    function disableResetBtn(disabled){
+        document.querySelector("#btnReset").disabled = disabled;
+    }
+
+})(window);
